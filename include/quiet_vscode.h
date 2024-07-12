f963f6779f3ed463026a22c6124fe36f3590ca52
MathNerdGamer <mathnerd@anothermathblog.com>
2024-07-12 01:23:34 -0500

    Put more stuff in quiet_vscode.h.

--- include/quiet_vscode.h
+++ include/quiet_vscode.h
@@ -7,14 +7,14 @@
 #ifdef __cplusplus
 // Sometimes VS Code doesn't realize a header is supposed to be C code,
 // so it complains when I use stuff that would be in namespace std (like
-// `std::size_t`)
+// `std::size_t`).
 using namespace std;
 #else
 
-#define constexpr const // `constexpr` is defined in C23
+#define constexpr const // `constexpr` is defined in C23.
 #define true 1          // `true` and
-#define false 0         // `false` are defined in C23
-#define nullptr NULL
+#define false 0         // `false` are defined in C23.
+#define nullptr NULL    // `nullptr` is also defined in C23.
 
 #endif // __cplusplus
 
